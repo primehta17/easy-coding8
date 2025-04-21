f874d3a980058cfa8af6376683d3e66a464e8263
Priyanka Mehta <primehta17@gmail.com>
2025-04-21 13:44:26 +0530

    ternary operator

---
+++ two.js
@@ -0,0 +1,6 @@
+// write a function to return the larger of two numbers using the ternary operator
+
+function findLarger(num1,num2){
+   return (num1>num2)?num1: num2;
+}
+console.log(findLarger(4,7))
